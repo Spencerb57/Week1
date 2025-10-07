987ad8caa492fff171dd608c2d90b0f8b20b1853
Spencerb57 <spencerjberry57@gmail.com>
2025-10-07 14:49:04 +0100

    Feat: Complete Week 1 lab exercises

---
+++ week1_lab.py
@@ -0,0 +1,18 @@
+print("Hola, Mundo!")
+
+studentName = "Spencer"
+
+studentAge = 18
+
+print(f"Student Name: {studentName}")
+
+print(f"Student Age: {studentName}")
+
+#Arithmetic
+
+num1 = 20
+num2 = 5
+
+total = num1 + num2
+#Should print 25
+print(total)
